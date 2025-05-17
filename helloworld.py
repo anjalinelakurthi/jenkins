df7585f60bb8fdd37bcae2eb12c3d6c91e3884f1
anjalinelakurthi <anjalinelakurthi28@gmail.com>
2025-05-17 22:37:57 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,6 @@
-
-print("Hello world")
-
-print("Update: This update will reflect once Jenkins auto builds after 5 minutes.")
-
-print("Update 2: Testing once more, just to be sure.")
-
-print("Update 3: Woah, we really are doing this!")
-
-print("Update 4: We are on a roll!")
+git init
+git remote add origin https://github.com/avinash-chauhan-mu/jenkins.git
+echo "print('Hello from GitHub!')" > helloworld.py
+git add .
+git commit -m "Initial commit with helloworld script"
+git push -u origin master
